7fbce47f0d2ca74a7c485b5f7dff598abdf416a6
szvmczek <szymon.lebensztejn.essa@gmail.com>
2025-12-18 12:24:12 +0100

    initial project setup

---
+++ src/test/java/pl/szvmczek/projecthuman/ProjectHumanApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.szvmczek.projecthuman;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectHumanApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
