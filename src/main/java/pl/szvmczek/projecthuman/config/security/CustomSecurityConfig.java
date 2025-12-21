e94863f31c296efe86e35d96f2582a9df64abe13
szvmczek <szymon.lebensztejn.essa@gmail.com>
2025-12-21 22:37:58 +0100

    update task button styles and hover effects, adjust task completion logic, and enhance security logout configuration

--- src/main/java/pl/szvmczek/projecthuman/config/security/CustomSecurityConfig.java
+++ src/main/java/pl/szvmczek/projecthuman/config/security/CustomSecurityConfig.java
@@ -2,12 +2,14 @@ package pl.szvmczek.projecthuman.config.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.servlet.util.matcher.PathPatternRequestMatcher;
 
 @Configuration
 public class CustomSecurityConfig {
@@ -27,7 +29,8 @@ public class CustomSecurityConfig {
                  .permitAll()
         )
         .logout(logout -> logout
-                .logoutUrl("/logout")
+                .logoutRequestMatcher(PathPatternRequestMatcher.pathPattern(HttpMethod.GET, "/logout"))
+                .logoutSuccessUrl("/login")
                 .permitAll()
         );
         http.csrf(csrf -> csrf.ignoringRequestMatchers("/h2-console/**"));
